c3a5c770a218e5482324df10200bcb6b02d6b937
Rodrigo <dalamangas@outlook.com>
2018-12-02 19:20:55 -0200

    FourSquare API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
 
+  state = {
+    locais: []
+  }
+
   componentDidMount() {
+    this.getLocais()
     this.loadMap()
   }
 
@@ -11,6 +17,26 @@ class App extends Component {
   loadMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyA6QsaRcYcvFayEmbuY2_X3iMREEWwiGZ4&callback=initMap")
     window.initMap = this.initMap
+  }
+
+  getLocais = () => {
+    const foursquare = "https://api.foursquare.com/v2/venues/explore?"
+    const parametros = {
+      client_id: "NQBSOREJ4CNOIV4THG1QNP3HWUA4PW2YRUDWEICCE13Z3MER",
+      client_secret: "N1B1VJED2BUVZENKCJR3UFGH5PFVXWHLDRPIK3YYBARCKOC1",
+      query: "drink",
+      near: "Ribeirao Preto",
+      v: "20181202"
+    }
+    axios.get(foursquare + new URLSearchParams(parametros))
+      .then(response => {
+        this.setState({
+          locais: response.data.response.groups[0].items
+        })
+      })
+      .catch(error => {
+        console.log("ERRO:" + error)
+      })
   }
 
   initMap = () => {
